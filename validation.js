d3df5c237e35346bc904d6c6739da968ae943721
rgmt88 <65685133+rgmt88@users.noreply.github.com>
2024-04-25 17:17:18 -0500

    added validatePassword and min/max length of zipcode

--- validation.js
+++ validation.js
@@ -1,10 +1,9 @@
 function validation() {
     liveValidation();
-    validateForm();
-    formAlert();
+    handleFormSubmission();
 }
 
-function formAlert() {
+function handleFormSubmission() {
     const form = document.getElementById('registrationForm');
     form.addEventListener('submit', function(event) {
         // Prevent form submission
@@ -19,9 +18,9 @@ function formAlert() {
 }
 
 function validateForm() {
-    let inputs = document.querySelectorAll('#registrationForm input');
+    let formInputs = document.querySelectorAll('#registrationForm input');
     let allValid = true;
-    inputs.forEach(input => {
+    formInputs.forEach(input => {
         if (!validateField(input)) {
             allValid = false;
         }
@@ -41,12 +40,19 @@ function liveValidation() {
 function validateField(input) {
     let isValid = true;
     let errorMessage = '';
+
     if (input.value.trim() === '') {
         isValid = false;
         errorMessage = 'This field is required';
     } else if (input.type === 'email' && !input.checkValidity()) {
         isValid = false;
         errorMessage = 'Please enter a valid email';
+    } else if (input.id === 'zipcode' && (input.value < 10000 || input.value > 19999)) {
+        isValid = false;
+        errorMessage = 'Please enter a valid number between [10000 - 20000]';
+    } else if (input.id === 'password' && !validatePassword(input.value)) {
+        isValid = false;
+        errorMessage = 'Password must be at least 8 characters long and contain at least one uppercase letter, one number, and one special character'
     } else if (input.id === 'confirmpassword' && input.value !== document.getElementById('password').value) {
         isValid = false;
         errorMessage = 'Passwords do not match';
@@ -55,15 +61,27 @@ function validateField(input) {
     let errorSpan = input.nextElementSibling;
     if (!isValid) {
         input.classList.add('error');
-        errorSpan.textContent = errorMessage;
-        errorSpan.style.display = 'block';
+        if (errorSpan) {
+            errorSpan.textContent = errorMessage;
+            errorSpan.style.display = 'block';
+        }
     } else {
         input.classList.remove('error');
-        errorSpan.textContent = '';
-        errorSpan.style.display = 'none';
+        if (errorSpan) {
+            errorSpan.textContent = '';
+            errorSpan.style.display = 'none';
+        }
     }
 
     return isValid;
 }
 
+function validatePassword(password) {
+    const passwordRegex = /^(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*()_+])[A-Za-z\d!@#$%^&*()_+]{8,}$/;
+    return passwordRegex.test(password);
+}
+
 validation();
+
+// Password min 8 with 1 Upper
+// Don’t forget to style validations with CSS by using the :valid and :invalid pseudo-classes!
